811fdecf04936a573b20a96d76e5f2b5e12b9f08
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-08-25 08:56:23 +0300

    commit tree c2d10e7dd5b5056885751c0c00a3de95b3209cf5

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,8 @@ Bundler.require
 require_relative '../lib/song'
 
 DB = { conn: SQLite3::Database.new("db/music.db") }
+# creating a new song instance
+Song.create_table
+kibali = Song.new(name: 'kibali', album: 'Florence Andenyi')
+# saving the newly created song instance attributes to the database.
+kibali.save
